4294a6234387aad7da545f6fffaa4c9d1ceab061
Goran Tenic <tenic.g@gmail.com>
2022-10-13 13:18:09 +0200

    made cat grid responsive

--- CatWikiApp/src/app/app.component.ts
+++ CatWikiApp/src/app/app.component.ts
@@ -4,23 +4,54 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public forecasts?: WeatherForecast[];
-
-  constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-  }
-
-  title = 'CatWikiApp';
+  breeds: Breed[] = [
+    {
+      id: 'toyg',
+      name: 'Toyger',
+      url: 'https://cdn2.thecatapi.com/images/O3F3_S1XN.jpg',
+    },
+    {
+      id: 'sava',
+      name: 'Savannah',
+      url: 'https://cdn2.thecatapi.com/images/a8nIYvs6S.jpg',
+    },
+    {
+      id: 'emau',
+      name: 'Egyptian Mau',
+      url: 'https://cdn2.thecatapi.com/images/TuSyTkt2n.jpg',
+    },
+    {
+      id: 'nebe',
+      name: 'Nebelung',
+      url: 'https://cdn2.thecatapi.com/images/OGTWqNNOt.jpg',
+    },
+  ];
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+export interface Breed {
+  id: string;
+  name: string;
+  url: string;
 }
+
+// export class AppComponent {
+//   public forecasts?: WeatherForecast[];
+
+//   constructor(http: HttpClient) {
+//     http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+//       this.forecasts = result;
+//     }, error => console.error(error));
+//   }
+
+//   title = 'CatWikiApp';
+// }
+
+// interface WeatherForecast {
+//   date: string;
+//   temperatureC: number;
+//   temperatureF: number;
+//   summary: string;
+// }
